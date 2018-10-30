db8413974d6cb36330d4fa412f0c5f0264cb07b2
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-30 11:36:07 +0800

    update pom.xml file info

--- kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/range/KotlinRange.kt
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/range/KotlinRange.kt
@@ -2,6 +2,7 @@ package com.laibao.kotlin.basic.range
 
 /**
  * @author laibao wang
+ *
  * Kotlin Ranges could be found in three forms:
  *      1: m..n
  *      2: m until n
@@ -13,21 +14,81 @@ package com.laibao.kotlin.basic.range
  */
 
 /**
- *
+ * m until n corresponds to the range[m,n) given m<n. By default, m is included in the range and n is excluded from the range
  */
 
 /**
- *
+ *  n downTo m corresponds to the range[n,m] given m<n. By default, m and n are included in the range
  */
 fun main(args: Array<String>) {
     testLoop()
+    testUtil()
+    testDownTo()
+    testCondition()
 }
 
 /**
- * Usage of Kotlin range m..n in Loop statement
+ * usage of kotlin range m..n in loop statement
  */
 fun testLoop() {
     for (element in 1 .. 100) {
         println("the element is $element")
+    }
+    println()
+}
+
+/**
+ * usage of kotlin range m until n  in loop statement
+ */
+fun testUtil() {
+    for (element in 1 until 20) {
+        println("the element is $element")
+    }
+    println()
+}
+
+/**
+ * usage of kotlin range n downTo m  in loop statement
+ */
+fun testDownTo() {
+    for(element in 40 downTo 1){
+        println("the element is $element")
+    }
+    println()
+}
+/**
+ * usage of kotlin range m..n in conditional statement
+ *                  and
+ * usage of kotlin range m until n  in conditional statement
+ *                  and
+ * usage of kotlin range n downTo m  in conditional statement
+ */
+fun testCondition() {
+    val number = 100
+    if (number in 50 .. 150) {
+        println(number.toString() + " is in the range 50..150")
+    }else {
+        println(number.toString() + " is not in the range 50..150")
+    }
+    println()
+
+    if(number in 1 until 100){
+        println(number.toString() + " is in the range")
+    }else {
+        println(number.toString() + " is not in the range")
+    }
+
+    println()
+    if(number in 1 until 101){
+        println(number.toString() + " is in the range")
+    }else {
+        println(number.toString() + " is not in the range")
+    }
+
+    println()
+    if(number in 200 downTo 50){
+        println(number.toString() + " is in the range")
+    }else{
+        println(number.toString() + " is not in the range")
     }
 }
