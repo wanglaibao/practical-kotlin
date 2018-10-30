d6a16a2ff29c6926014ab5f6de009d6bcc522ccb
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-30 09:20:42 +0800

    update pom.xml file info

---
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/loop/KotlinWhileAndDoWhileLoop.kt
@@ -0,0 +1,53 @@
+package com.laibao.kotlin.basic.loop
+
+/**
+ * @author laibao wang
+ *
+ */
+
+
+/**
+ *  kotlin while statement is used to repeat a task,
+ *  checking the condition priorly for each iteration before executing the iterative block
+ */
+
+/**
+ * kotlin do-while loop is the same as while loop,
+ * except that the condition is not checked for the first time before executing the iterative block.
+ * therefore it is guaranteed that the iterative block is executed at least once
+ */
+
+fun main(args: Array<String>) {
+    testWhile()
+    testWhileWithBreak()
+    testDoWhile()
+}
+
+fun testWhile():Unit {
+    var number = 0
+    while (number < 10) {
+        println("hello, the number is $number")
+        number += 1
+    }
+    println()
+}
+
+fun testWhileWithBreak():Unit {
+    var number = 100
+    while (true) {
+        number += 1
+        println("hi, the number is $number")
+        if (number > 120) {
+            break
+        }
+    }
+    println()
+}
+
+fun testDoWhile(): Unit {
+    var number = 1000
+    do {
+        println("heheh, the number is $number")
+        number += 1
+    }while (number < 1020)
+}
