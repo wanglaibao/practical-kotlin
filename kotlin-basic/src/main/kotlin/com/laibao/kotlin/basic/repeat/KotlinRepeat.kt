b01c0c724c8af632e0c5fa9fd9d452648ede071c
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-30 10:20:02 +0800

    update pom.xml file info

---
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/repeat/KotlinRepeat.kt
@@ -0,0 +1,15 @@
+package com.laibao.kotlin.basic.repeat
+
+/**
+ * @author laibao wang
+ */
+
+fun main(args: Array<String>) {
+    testRePeat()
+}
+
+fun testRePeat() {
+    repeat(100){
+        println("hello,nice to meet you,金戈")
+    }
+}
