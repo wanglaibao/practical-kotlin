04be11415e4fb9558976746e465dd0b7dedd79c3
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-30 08:05:49 +0800

    update pom.xml file info

--- kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/KotlinMainFunction.kt
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/KotlinMainFunction.kt
@@ -11,6 +11,9 @@ fun printUser(user: User): Unit {
     println("user name is ${user.name} and user age is ${user.age}")
 }
 
+/**
+ * main() function is an entry point to Kotlin program
+ */
 fun main(args: Array<String>) {
     val user: User = User("金戈",30)
     printUser(user)
