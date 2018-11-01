760e8d430bf902bc8b5b6df9e28d21cfef32a072
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-01 11:13:05 +0800

    update pom.xml file info

--- spring-boot-kotlin-demo/src/main/kotlin/com/laibao/kotlin/springbootkotlin/demo/SpringBootKotlinDemoApplication.kt
+++ spring-boot-kotlin-demo/src/main/kotlin/com/laibao/kotlin/springbootkotlin/demo/SpringBootKotlinDemoApplication.kt
@@ -1,10 +1,15 @@
 package com.laibao.kotlin.springbootkotlin.demo
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class SpringBootKotlinDemoApplication {
+
+    private val logger: Logger = LoggerFactory.getLogger(SpringBootKotlinDemoApplication::class.java)
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
