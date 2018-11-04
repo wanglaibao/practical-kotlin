535ad4910053dc4493c3f07887ec555fa3948cbe
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-04 10:28:25 +0800

    update pom.xml file info

---
+++ smart-decision-system/src/main/kotlin/com/laibao/smart/decision/domain/SysProvince.kt
@@ -0,0 +1,8 @@
+package com.laibao.smart.decision.domain
+
+import java.io.Serializable
+
+data class SysProvince (val provinceId: String,
+                        val province: String): Serializable {
+
+}
