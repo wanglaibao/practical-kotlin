f71d421d8578c6432e10b751da2baa0a77c4262e
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-04 12:02:07 +0800

    update pom.xml file info

--- smart-decision-system/src/main/kotlin/com/laibao/smart/decision/service/impl/SysProvinceServiceImpl.kt
+++ smart-decision-system/src/main/kotlin/com/laibao/smart/decision/service/impl/SysProvinceServiceImpl.kt
@@ -3,7 +3,6 @@ package com.drong.smart.decision.service.impl
 import com.laibao.smart.decision.domain.SysProvince
 import com.laibao.smart.decision.mapper.SysProvinceMapper
 import com.laibao.smart.decision.service.SysProvinceService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service("sysProvinceService")
