9c858862bdc9a9bf19143f5b83bb13ec94fbb748
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-05 07:17:11 +0800

    update pom.xml file info

--- smart-decision-system/src/test/kotlin/com/laibao/smart/decision/service/test/BaseServiceTest.kt
+++ smart-decision-system/src/test/kotlin/com/laibao/smart/decision/service/test/BaseServiceTest.kt
@@ -1,4 +1,4 @@
-package com.drong.smart.decision.service
+package com.laibao.smart.decision.service.test
 
 import com.laibao.smart.decision.SmartDecisionSysApplication
 import org.junit.Test
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = arrayOf(SmartDecisionSysApplication::class))
-class BaseServiceTest {
+class BaseServiceTest{
 
     @Test
     fun contextLoads() {
