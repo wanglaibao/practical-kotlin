218391ec4ed3b104779f64aa3100d8dd1cf21542
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-04 13:25:16 +0800

    update pom.xml file info

--- smart-decision-system/src/main/kotlin/com/laibao/smart/decision/domain/UserArea.kt
+++ smart-decision-system/src/main/kotlin/com/laibao/smart/decision/domain/UserArea.kt
@@ -2,11 +2,11 @@ package com.laibao.smart.decision.domain
 
 import java.io.Serializable
 
-data class UserArea ( val id: Long,
-                      val userId: Long,
-                      val provinceId: String,
-                      val cityId: String,
-                      val districtId: String,
-                      val status: Byte): Serializable {
+data class UserArea ( var id: Long? = null,
+                      var userId: Long? = null,
+                      var provinceId: String? = null,
+                      var cityId: String? = null,
+                      var districtId: String? = null,
+                      var status: Byte? = null): Serializable {
 
 }
--- smart-decision-system/src/main/kotlin/com/laibao/smart/decision/service/impl/UserAreaServiceImpl.kt
+++ smart-decision-system/src/main/kotlin/com/laibao/smart/decision/service/impl/UserAreaServiceImpl.kt
@@ -1,176 +1,51 @@
 package com.drong.smart.decision.service.impl
 
-import com.drong.smart.decision.bean.vo.*
-import com.drong.smart.decision.domain.*
-import com.drong.smart.decision.mapper.UserAreaMapper
-import com.drong.smart.decision.service.SysCityService
-import com.drong.smart.decision.service.SysDistrictService
-import com.drong.smart.decision.service.SysProvinceService
-import com.drong.smart.decision.service.UserAreaService
-import org.apache.commons.collections.CollectionUtils
-import org.apache.commons.lang3.StringUtils
+import com.laibao.smart.decision.domain.SysCity
+import com.laibao.smart.decision.domain.SysDistrict
+import com.laibao.smart.decision.domain.UserArea
+import com.laibao.smart.decision.mapper.UserAreaMapper
+import com.laibao.smart.decision.service.SysCityService
+import com.laibao.smart.decision.service.SysDistrictService
+import com.laibao.smart.decision.service.SysProvinceService
+import com.laibao.smart.decision.service.UserAreaService
+import com.laibao.smart.decision.vo.SysCityVO
+import com.laibao.smart.decision.vo.SysDistrictVO
+import com.laibao.smart.decision.vo.SysProvinceVO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 import java.util.ArrayList
 
-/**
- * @author laibao wang
- * @date 2018-05-22
- * @version 1.0
- */
 @Service("userAreaService")
-class UserAreaServiceImpl : UserAreaService {
+class UserAreaServiceImpl (val userAreaMapper: UserAreaMapper): UserAreaService {
+
+//    @Autowired
+//    private val userAreaMapper: UserAreaMapper? = null
 
     @Autowired
-    private val userAreaMapper: UserAreaMapper? = null
+    private lateinit var sysProvinceService: SysProvinceService
+
+//    @Autowired
+//    private val sysProvinceService: SysProvinceService? = null
 
     @Autowired
-    private val sysProvinceService: SysProvinceService? = null
+    private lateinit var sysCityService: SysCityService
+
+//    @Autowired
+//    private val sysCityService: SysCityService? = null
 
     @Autowired
-    private val sysCityService: SysCityService? = null
+    private lateinit var sysDistrictService: SysDistrictService
+//    @Autowired
+//    private val sysDistrictService: SysDistrictService? = null
 
-    @Autowired
-    private val sysDistrictService: SysDistrictService? = null
-
-    fun selectByExample(example: UserAreaExample): List<UserArea> {
-        return userAreaMapper!!.selectByExample(example)
+    fun selectByPrimaryKey(id: Long): UserArea {
+        return userAreaMapper.selectByPrimaryKey(id)
     }
 
-    fun selectByPrimaryKey(id: Long?): UserArea {
-        return userAreaMapper!!.selectByPrimaryKey(id)
+    fun selectUserAreaListByUid(uid: Long): List<UserArea> {
+        return userAreaMapper.selectUserAreaListByUid(uid)
     }
-
-    fun selectUserAreaListByUid(uid: Long?): List<UserArea> {
-        return userAreaMapper!!.selectUserAreaListByUid(uid)
-    }
-
-    /**
-     * 获取全国所有省市区情况
-     * @return List<SysProvinceVO>
-    </SysProvinceVO> */
-    val allAreaList: List<SysProvinceVO>
-        get() {
-            val provinceExample = SysProvinceExample()
-            val provinceList = sysProvinceService!!.selectByExample(provinceExample)
-
-            val cityExample = SysCityExample()
-            val cityList = sysCityService!!.selectByExample(cityExample)
-
-            val districtExample = SysDistrictExample()
-            val districtList = sysDistrictService!!.selectByExample(districtExample)
-
-            val provinceVOList = ArrayList()
-            for (province in provinceList) {
-                val provinceId = province.getProvinceId()
-                val provinceName = province.getProvince()
-                val provinceVO = SysProvinceVO()
-                provinceVO.setId(provinceId)
-                provinceVO.setName(provinceName)
-                val cityVOList = ArrayList()
-                for (city in cityList) {
-                    if (city.getProvinceId().equals(provinceId)) {
-                        val cityVO = SysCityVO()
-                        val cityId = city.getCityId()
-                        val cityName = city.getCity()
-                        val districtVOList = ArrayList()
-                        for (district in districtList) {
-                            val districtId = district.getDistrictId()
-                            val districtName = district.getDistrict()
-                            if (district.getCityId().equals(cityId)) {
-                                val sysDistrictVO = SysDistrictVO()
-                                sysDistrictVO.setId(districtId)
-                                sysDistrictVO.setName(districtName)
-                                districtVOList.add(sysDistrictVO)
-                            }
-                        }
-                        cityVO.setId(cityId)
-                        cityVO.setName(cityName)
-                        cityVO.setChild(districtVOList)
-                        cityVOList.add(cityVO)
-                    }
-                }
-                provinceVO.setChild(cityVOList)
-                provinceVOList.add(provinceVO)
-            }
-            return provinceVOList
-        }
-
-
-    /**
-     * 根据provinceId获取该省所有市区情况
-     * @return SysProvinceVO
-     */
-    fun getAreaByProvinceId(provinceId: String): SysProvinceVO {
-        val sysProvince = sysProvinceService!!.selectByPrimaryKey(provinceId)
-        val provinceVO = SysProvinceVO()
-        provinceVO.setId(sysProvince.getProvinceId())
-        provinceVO.setName(sysProvince.getProvince())
-        //城市为空的话取该省下面的所有城市
-        val cityList = sysCityService!!.selectByProvinceId(provinceId)
-        val cityVOList = ArrayList()
-        for (city in cityList) {
-            val cityVO = SysCityVO()
-            val cityId = city.getCityId()
-            val cityName = city.getCity()
-            cityVO.setId(cityId)
-            cityVO.setName(cityName)
-            val districtVOList = ArrayList()
-            val sysDistrictList = sysDistrictService!!.selectByCityId(cityId)
-            for (district in sysDistrictList) {
-                val sysDistrictVO = SysDistrictVO()
-                val districtId = district.getDistrictId()
-                val districtName = district.getDistrict()
-                sysDistrictVO.setId(districtId)
-                sysDistrictVO.setName(districtName)
-                districtVOList.add(sysDistrictVO)
-            }
-            cityVO.setChild(districtVOList)
-            cityVOList.add(cityVO)
-        }
-        provinceVO.setChild(cityVOList)
-        return provinceVO
-    }
-
-
-    /**
-     * 根据provinceId,cityId获取该市所有区情况
-     * @param provinceId
-     * @param cityId
-     * @return SysProvinceVO
-     */
-    fun getAreaByProvinceIdAndCityId(provinceId: String, cityId: String): SysProvinceVO {
-        val provinceVO = SysProvinceVO()
-        val sysProvince = sysProvinceService!!.selectByPrimaryKey(provinceId)
-        provinceVO.setId(sysProvince.getProvinceId())
-        provinceVO.setName(sysProvince.getProvince())
-
-        val key = SysCity()
-        key.setProvinceId(provinceId)
-        key.setCityId(cityId)
-        val city = sysCityService!!.selectByPrimaryKey(key)
-
-        val cityVOList = ArrayList()
-        val cityVO = SysCityVO()
-        cityVO.setId(cityId)
-        cityVO.setName(city.getCity())
-        val districtVOList = ArrayList()
-        val sysDistrictList = sysDistrictService!!.selectByCityId(cityId)
-        for (sysDistrict in sysDistrictList) {
-            val sysDistrictVO = SysDistrictVO()
-            val districtId = sysDistrict.getDistrictId()
-            val districtName = sysDistrict.getDistrict()
-            sysDistrictVO.setId(districtId)
-            sysDistrictVO.setName(districtName)
-            districtVOList.add(sysDistrictVO)
-        }
-        cityVO.setChild(districtVOList)
-        cityVOList.add(cityVO)
-        provinceVO.setChild(cityVOList)
-        return provinceVO
-    }
-
 
     /**
      * 根据provinceId,cityId,districtId获取对应的省市区
@@ -181,35 +56,48 @@ class UserAreaServiceImpl : UserAreaService {
      */
     fun getAreaByProvinceIdAndCityIdAndDistrictId(provinceId: String, cityId: String, districtId: String): SysProvinceVO {
         val provinceVO = SysProvinceVO()
-        val sysProvince = sysProvinceService!!.selectByPrimaryKey(provinceId)
-        provinceVO.setId(sysProvince.getProvinceId())
-        provinceVO.setName(sysProvince.getProvince())
+        val sysProvince = sysProvinceService.selectByPrimaryKey(provinceId)
+        //provinceVO.setId(sysProvince.getProvinceId())
+        //provinceVO.setName(sysProvince.getProvince())
+        provinceVO.id = sysProvince.provinceId
+        provinceVO.name = sysProvince.province
 
-        val key = SysCity()
-        key.setProvinceId(provinceId)
-        key.setCityId(cityId)
-        val sysCity = sysCityService!!.selectByPrimaryKey(key)
-        val cityVOList = ArrayList()
+        val key: SysCity = SysCity()
+        //key.setProvinceId(provinceId)
+        //key.setCityId(cityId)
+        key.provinceId = provinceId
+        key.cityId = cityId
+
+        val sysCity = sysCityService.selectByPrimaryKey(key)
+        val cityVOList: MutableList<SysCityVO> = mutableListOf<SysCityVO>()
 
         val cityVO = SysCityVO()
-        cityVO.setId(cityId)
-        cityVO.setName(sysCity.getCity())
-        val districtVOList = ArrayList()
+        cityVO.id = cityId
+        cityVO.name = sysCity.city
+        //cityVO.setName(sysCity.getCity())
+
+        val districtVOList:MutableList<SysDistrictVO> = mutableListOf<SysDistrictVO>()
 
         val districtKey = SysDistrict()
-        districtKey.setCityId(cityId)
-        districtKey.setDistrictId(districtId)
+        districtKey.cityId = cityId
+        districtKey.districtId = districtId
+        //districtKey.setCityId(cityId)
+        //districtKey.setDistrictId(districtId)
 
-        val sysDistrict = sysDistrictService!!.selectByPrimaryKey(districtKey)
+        val sysDistrict = sysDistrictService.selectByPrimaryKey(districtKey)
         val sysDistrictVO = SysDistrictVO()
-        sysDistrictVO.setId(districtId)
-        sysDistrictVO.setName(sysDistrict.getDistrict())
+        sysDistrictVO.id = districtId
+        sysDistrictVO.name = sysDistrict.district
+        //sysDistrictVO.setId(districtId)
+        //sysDistrictVO.setName(sysDistrict.getDistrict())
         districtVOList.add(sysDistrictVO)
 
-        cityVO.setChild(districtVOList)
+        cityVO.child = districtVOList
+        //cityVO.setChild(districtVOList)
 
         cityVOList.add(cityVO)
-        provinceVO.setChild(cityVOList)
+        provinceVO.child = cityVOList
+        //provinceVO.setChild(cityVOList)
         return provinceVO
     }
 
@@ -218,6 +106,7 @@ class UserAreaServiceImpl : UserAreaService {
      * @param uid
      * @return UserAreaVO
      */
+    /*
     fun getUserUserAreaVOByUid(uid: Long?): UserAreaVO {
         val userAreaList = selectUserAreaListByUid(uid)
         val userAreaVO = UserAreaVO()
@@ -261,5 +150,16 @@ class UserAreaServiceImpl : UserAreaService {
         userAreaVO.setBankArea(stringBuilder.toString())
         userAreaVO.setCaseAreaVOList(caseAreaVOList)
         return userAreaVO
+    }
+    */
+
+    override fun selectByPrimaryKey(id: Long?): UserArea {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return UserArea()
+    }
+
+    override fun selectUserAreaListByUid(uid: Long?): List<UserArea> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ArrayList<UserArea>()
     }
 }
