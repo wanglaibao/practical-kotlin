99fd362b92de0e86ef4934235afe784965b4927b
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-05 09:39:45 +0800

    update pom.xml file info

--- kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/HelloWorld.kt
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/HelloWorld.kt
@@ -1,8 +1,12 @@
 package com.laibao.kotlin.basic
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
  * @author laibao wang
  */
 fun main(args: Array<String>) {
     println("Hello World !")
+    println(SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()))
 }
