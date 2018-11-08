8cca22f24a671d6c9dc749e99080c9683b3b9b55
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-08 19:14:50 +0800

    update pom.xml file info

--- kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/HelloWorld.kt
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/HelloWorld.kt
@@ -9,4 +9,18 @@ import java.util.*
 fun main(args: Array<String>) {
     println("Hello World !")
     println(SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()))
+    testIfConditions()
+}
+
+fun testIfConditions() {
+    val age = 45
+    if (age >= 60) {
+        println("老年人")
+    }else if (age >= 40 && age < 60) {
+        println("中年人")
+    }else if (age >= 20 && age < 40) {
+        println("青年人")
+    } else{
+        println("少年人")
+    }
 }
