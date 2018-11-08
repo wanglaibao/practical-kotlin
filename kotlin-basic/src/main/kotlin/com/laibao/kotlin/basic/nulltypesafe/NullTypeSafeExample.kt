9c9888417a6062b72b90500b629852c502f47d81
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-08 11:42:32 +0800

    update pom.xml file info

--- kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/nulltypesafe/NullTypeSafeExample.kt
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/nulltypesafe/NullTypeSafeExample.kt
@@ -15,4 +15,17 @@ fun main(args: Array<String>) {
     }else {
         println("是数字类型的字符串")
     }
+
+
+    var aStr: String = "afdasdfasf"
+    var bStr : String? ="sadfasfdas"
+    //错误,aStr不接受null
+    //aStr = null
+    //正确
+    bStr = null
+    //编译通过，aStr不可能为null，运行时不可能导致NPE
+    println(aStr.length)
+    //编译不能通过,不可能导致NPE
+    //println(bStr.lenqth)
+
 }
