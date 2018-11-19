0b2f40b90ece75003b0380f8a6e176a0e782cf37
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-19 11:36:50 +0800

    update pom.xml file info

--- kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/range/KotlinRange.kt
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/range/KotlinRange.kt
@@ -25,6 +25,35 @@ fun main(args: Array<String>) {
     testUtil()
     testDownTo()
     testCondition()
+    println()
+
+    val x = 10
+    val y = 9
+    if (x in 1..y+1) {
+        println("fits in range")
+    }
+    println()
+
+    val list = listOf("a", "b", "c")
+    if (-1 !in 0..list.lastIndex) {
+        println("-1 is out of range")
+    }
+    if (list.size !in list.indices) {
+        println("list size is out of valid list indices range too")
+    }
+    println()
+
+    for (x in 1..5) {
+        print(x)
+    }
+    println()
+    for (x in 1..10 step 2) {
+        print(x)
+    }
+    println()
+    for (x in 9 downTo 0 step 3) {
+        print(x)
+    }
 }
 
 /**
