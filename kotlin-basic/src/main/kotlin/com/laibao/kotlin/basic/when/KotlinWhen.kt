4ea5f6fe168ffe45da2c000538989c566a1063ce
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-19 11:31:11 +0800

    update pom.xml file info

--- kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/when/KotlinWhen.kt
+++ kotlin-basic/src/main/kotlin/com/laibao/kotlin/basic/when/KotlinWhen.kt
@@ -122,6 +122,14 @@ fun testPrintWeekDayWithAnyType(element: Any):Unit {
 }
 
 
+fun describe(obj: Any): String = when (obj) {
+                                            1 -> "One"
+                                            "Hello" -> "Greeting"
+                                            is Long -> "Long"
+                                            !is String -> "Not a string"
+                                            else -> "Unknown"
+                                }
+
 
 
 
