23398fdb3a235e6ee3a910024658a95c0f1d9168
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-10 13:23:03 +0800

    update pom.xml file info

--- designpattern-in-kotlin/src/main/kotlin/com/laibao/kotlin/designpattern/singleton/SingletonExample.kt
+++ designpattern-in-kotlin/src/main/kotlin/com/laibao/kotlin/designpattern/singleton/SingletonExample.kt
@@ -2,16 +2,38 @@ package com.laibao.kotlin.designpattern.singleton
 
 import java.util.concurrent.atomic.AtomicLong
 
-fun main() {
-    for (i in 1..10000) {
-        println(CounterSingleton.increment())
-    }
-}
+/**
+ * The singleton pattern ensures that only one object of a particular class is ever created.
+ * All further references to objects of the singleton class refer to the same underlying instance.
+ *
+ */
 
 /**
  *  Just like regular classes,
  *  objects can extend other classes and implement interfaces
  */
+
+fun main() {
+    println("Start")
+    PrinterDriver.print()
+    PrinterDriver.print()
+    PrinterDriver.print()
+    PrinterDriver.print()
+    println()
+    for (i in 1..10) {
+        println(CounterSingleton.increment())
+    }
+}
+
+object PrinterDriver {
+    init {
+        println("Initializing with object: $this")
+    }
+
+    fun print() = println("Printing with object: $this")
+}
+
+
 object CounterSingleton {
 
     init {
