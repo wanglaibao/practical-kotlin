703f9aa3493590b083da4e5888f9c3b85ea086ae
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-29 23:45:34 +0800

    update pom.xml file info

--- simple-kotlin-mybatis/src/main/kotlin/com/laibao/kotlin/simple/mybatis/controller/UserController.kt
+++ simple-kotlin-mybatis/src/main/kotlin/com/laibao/kotlin/simple/mybatis/controller/UserController.kt
@@ -1,9 +1,8 @@
 package com.laibao.kotlin.simple.mybatis.controller
 
 import com.laibao.kotlin.simple.mybatis.domain.User
-import com.laibao.kotlin.simple.mybatis.mapper.UserMapper
 import com.laibao.kotlin.simple.mybatis.service.UserService
-import org.apache.ibatis.annotations.Param
+import kotlinx.coroutines.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.*
  * @author laibao wang
  */
 @RestController
-class UserController {
+class UserController (){
 
 //    @Autowired
 //    lateinit var userMapper: UserMapper
@@ -20,37 +19,50 @@ class UserController {
     private lateinit var userService: UserService
 
     @GetMapping("/all")
-    fun getAllUser() : List<User> {
-        //测试通过了
-        val userList: List<User> = userService.getAllUser()
-        return userList
+    fun getAllUser() : List<User> = runBlocking{
+        GlobalScope.async{
+            //测试通过了
+            val userList: List<User> = userService.getAllUser()
+            userList
+        }.await()
     }
 
     @GetMapping("/user/{id}")
-    fun getUserById(@PathVariable id : Long) : User {
-        //测试通过了
-        val user:User = userService.getUserById(id)
-        return user
+    fun getUserById(@PathVariable id : Long) : User = runBlocking{
+        GlobalScope.async{
+            //测试通过了
+            val user:User = userService.getUserById(id)
+            //return user
+            user
+        }.await()
     }
 
     @PostMapping("/user")
-    fun addUser(@RequestBody user:User) : List<User> {
-        //测试通过了
-        userService.insertUser(user)
-        return getAllUser()
+    fun addUser(@RequestBody user:User) : List<User> = runBlocking{
+        GlobalScope.async{
+            //测试通过了
+            userService.insertUser(user)
+            //return getAllUser()
+            userService.getAllUser()
+        }.await()
     }
 
     @PutMapping("/user")
-    fun updateUserById(@RequestBody user: User) : List<User> {
-        //测试通过了
-        userService.modifyUser(user)
-        return getAllUser()
+    fun updateUserById(@RequestBody user: User) : List<User> = runBlocking{
+        GlobalScope.async{
+            //测试通过了
+            userService.modifyUser(user)
+            //return getAllUser()
+            userService.getAllUser()
+        }.await()
     }
 
     @DeleteMapping("user")
-    fun removeUserById(@RequestBody user:User) : List<User> {
-        userService.removeUserById(user.id)
-        return getAllUser()
+    fun removeUserById(@RequestBody user:User) : List<User> = runBlocking{
+        GlobalScope.async{
+            userService.removeUserById(user.id)
+            userService.getAllUser()
+        }.await()
     }
 
 }
